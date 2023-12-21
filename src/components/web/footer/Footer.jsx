decf203367a1dc0b3f16546bc1e8d15c18e3af2e
maryam sholi <marymarie905@gmail.com>
2023-12-21 22:56:48 +0200

    ecommerce lastTask

--- src/components/web/footer/Footer.jsx
+++ src/components/web/footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 
     <div className=" my-5">
       <footer className="text-center text-lg-start text-white bg-danger" style={{ backgroundColor: '#45526e' }}>
-        <div className="container p-4 pb-0">
+        <div className="container p-2 pb-0">
           <section className>
             <div className="row">
               <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
@@ -46,23 +46,19 @@ export default function Footer() {
                 <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
                 <p><i className="fas fa-home mr-3" /> New York, NY 10012, US</p>
                 <p><i className="fas fa-envelope mr-3" /> info@gmail.com</p>
-                <p><i className="fas fa-phone mr-3" /> + 01 234 567 88</p>
-                <p><i className="fas fa-print mr-3" /> + 01 234 567 89</p>
               </div>
             </div>
           </section>
           <hr className="my-3" />
           <section className="p-3 pt-0">
-            <div className="row d-flex align-items-center">
+            <div className="row d-flex align-items-center ">
               <div className="col-md-7 col-lg-8 text-center text-md-start">
-                <div className="p-3">
+                <div className="p-3 ">
                   © 2023Copyright:
                   <a className="text-white" href="https://mdbootstrap.com/">Maryam Sholi</a>
                 </div>
               </div>
               <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-                <a className="btn btn-outline-light btn-floating m-1" role="button"><i className="fab fa-facebook-f" /></a>
-                {/* Twitter */}
  
               </div>
             </div>
